5fecbd1e8e3a4cd2112ab4d2bc3db6abdb1129f6
Sevada20 <poxosyansevada@gmail.com>
2025-05-01 20:56:17 +0400

    feat(header): update developer experience and format text
    - Add 2+ years of experience to frontend developer title
    - Format text for better readability
    - Adjust HTML structure for consistency

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,20 +4,22 @@ import resume from "../../assets/Sevada_Poghosyan_CV.pdf";
 
 const Header = () => {
   return (
-    <header className='header'>
-      <div className='header__wrapper'>
-        <h1 className='header__title'>
+    <header className="header">
+      <div className="header__wrapper">
+        <h1 className="header__title">
           <strong>
             Hi, my name is <em>Sevada</em>
           </strong>
-          <br />a frontend developer
+          <br />a frontend developer (2+ years of experience)
         </h1>
-        <div className='header__text'>
+        <div className="header__text">
           <p>
-          I specialize in crafting clean, intuitive, and user-centered web experiences. I'm passionate about building high-quality software that makes a meaningful impact on users' lives.
+            I specialize in crafting clean, intuitive, and user-centered web
+            experiences. I'm passionate about building high-quality software
+            that makes a meaningful impact on users' lives.
           </p>
         </div>
-        <a href={resume} download='Resume' className='btn'>
+        <a href={resume} download="Resume" className="btn">
           Download CV
         </a>
       </div>
